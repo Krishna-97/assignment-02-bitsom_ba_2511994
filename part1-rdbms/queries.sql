b8f572cc9b241cf81440acf3cf2f4d1add991b02
Krishna-97 <tkkoushick22@gmail.com>
2026-03-17 22:06:53 +0530

    add queries.sql file

---
+++ part1-rdbms/queries.sql
@@ -0,0 +1,35 @@
+use my_demo_db;
+show tables;
+-- Q1: List all customers from Mumbai along with their total order value
+SELECT c.customer_name, SUM(o.quantity * p.unit_price) AS total_order_value FROM Customer c JOIN Orders o ON c.customer_id = o.customer_id JOIN Product p ON o.product_id = p.product_id WHERE c.customer_city = 'Mumbai'
+GROUP BY c.customer_id,c.customer_name;
+
+-- Q2: Find the top 3 products by total quantity sold
+SELECT p.product_name, SUM(o.quantity) AS total_quantity_sold FROM Product p
+JOIN Orders o ON p.product_id = o.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY total_quantity_sold DESC
+LIMIT 3;
+
+-- Q3: List all sales representatives and the number of unique customers they have handled
+SELECT s.sales_rep_name, COUNT(DISTINCT o.customer_id) AS unique_customers_count FROM SalesRep s
+LEFT JOIN Orders o ON s.sales_rep_id = o.sales_rep_id
+GROUP BY s.sales_rep_id, s.sales_rep_name;
+
+-- Q4: Find all orders where the total value exceeds 10,000, sorted by value descending
+SELECT o.order_id,(o.quantity * p.unit_price) AS order_value
+FROM Orders o JOIN Product p ON o.product_id = p.product_id
+WHERE (o.quantity * p.unit_price) > 10000
+ORDER BY order_value DESC;
+
+-- Q5: Identify any products that have never been ordered
+SELECT p.product_id, p.product_name FROM Product p
+LEFT JOIN Orders o ON p.product_id = o.product_id
+WHERE o.order_id IS NULL;
+
+
+
+
+
+
+
